8060f0b8ec0021b9dc2df5b91dffe03fe00fcad4
Alfonso Alba <aalbagarcia@gmail.com>
2013-01-28 11:14:47 +0100

    Configurando root en config/routes.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
     @posts = Post.all
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html # index.orig.html.erb
       format.json { render json: @posts }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -52,8 +52,8 @@ Blog::Application.routes.draw do
   #   end
 
   # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # just remember to delete public/index.orig.html.
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
